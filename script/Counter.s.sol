d48512930bf7c4fc88e5bbd3cd473c3acafbcefa
Chloe Tan <chloetan@Chloe-Tans-MacBook-Pro.local>
2023-10-24 16:29:08 +0800

    first commit

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,11 +2,15 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
+import {EtherGame} from "../src/Counter.sol"; 
+import {Attack} from "../src/Counter.sol"; 
+
 
 contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast();
+
     }
 }
